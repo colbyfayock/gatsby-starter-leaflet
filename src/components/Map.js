2ff423bab0b1f16bcf64689835933c64be66df25
Colby Fayock <fayock@gmail.com>
2019-05-27 17:35:27 -0400

    fixing build, removing unneeded packages

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'proj4';
-import 'proj4leaflet';
 import 'leaflet/dist/leaflet.css'; // This needs to be included for the map to actually work when compiled
 import L from 'leaflet';
 import { Map as BaseMap, TileLayer, ZoomControl } from 'react-leaflet';
-
-// To get around an issue with the default icon not being set up right between using React
-// and importing the leaflet library, we need to reset the image imports
-// See https://github.com/PaulLeCam/react-leaflet/issues/453#issuecomment-410450387
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
 
 const Map = ({ children, className, center = [0, 0], zoom = 2 }) => {
   const mapClassName = `map ${className || ''}`;
@@ -28,6 +14,18 @@ const Map = ({ children, className, center = [0, 0], zoom = 2 }) => {
       </div>
     );
   }
+
+  // To get around an issue with the default icon not being set up right between using React
+  // and importing the leaflet library, we need to reset the image imports
+  // See https://github.com/PaulLeCam/react-leaflet/issues/453#issuecomment-410450387
+
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  });
 
   const mapSettings = {
     className: 'map-base',
