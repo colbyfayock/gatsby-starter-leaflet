5c238bebd062f4b57d3dbccd7d2aad4f930d4533
Colby Fayock <fayock@gmail.com>
2019-10-21 21:56:39 -0400

    changing zoom to flyto, adding center to make consistent zoom results

--- src/lib/map.js
+++ src/lib/map.js
@@ -1,26 +1,43 @@
 /**
- * promiseToZoomIn
+ * promiseToFlyTo
  * @description
  */
 
-export function promiseToZoomIn( map, zoom ) {
+export function promiseToFlyTo( map, { zoom, center }) {
   return new Promise(( resolve, reject ) => {
+    const baseError = 'Failed to fly to area';
+
     if ( !map.flyTo ) {
-      reject( 'Failed to zoom: no flyTo method on map' );
+      reject( `${baseError}: no flyTo method on map` );
     }
 
     if ( typeof zoom !== 'number' ) {
-      reject( `Failed to zoom: zoom invalid number ${zoom}` );
+      reject( `${baseError}: zoom invalid number ${zoom}` );
     }
 
-    const center = map.getCenter();
+    const mapCenter = center || map.getCenter();
+    const mapZoom = zoom || map.getZoom();
 
-    map.flyTo( center, zoom, {
+    map.flyTo( mapCenter, mapZoom, {
       duration: 2
     });
 
     map.once( 'moveend', () => {
       resolve();
     });
+  });
+}
+
+/**
+ * getCurrentLocation
+ * @description
+ */
+
+export function getCurrentLocation() {
+  return new Promise(( resolve, reject ) => {
+    navigator.geolocation.getCurrentPosition(
+      ( pos ) => resolve({ lat: pos.coords.latitude, lng: pos.coords.longitude }),
+      ( err ) => reject( err )
+    );
   });
 }
