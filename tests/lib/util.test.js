d20d304fd1ee917055c4f083c7b1237dc8c26928
colbyfayock <fayock@gmail.com>
2021-09-06 21:10:02 -0400

    fixing jest test

--- tests/lib/util.test.js
+++ tests/lib/util.test.js
@@ -2,34 +2,38 @@ import { isDomAvailable } from "lib/util";
 
 describe("lib::util", () => {
   describe("isDomAvailable", () => {
-    let windowSpy;
-
-    beforeEach(() => {
-      windowSpy = jest.spyOn(global, "window", "get");
-    });
+    let originalWindow = { ...global.window };
 
     afterEach(() => {
-      windowSpy.mockRestore();
+      Object.defineProperty(global, "window", {
+        value: originalWindow,
+        writable: true,
+      });
     });
 
     it("should return true when document create is available", () => {
-      windowSpy.mockImplementation(() => ({
-        document: {
-          createElement: () => {},
+      Object.defineProperty(global, "window", {
+        value: {
+          document: {
+            createElement: () => {},
+          },
         },
-      }));
+        writable: true,
+      });
       expect(isDomAvailable()).toEqual(true);
     });
 
     it("should return false with window not available", () => {
-      windowSpy.mockImplementation(() => undefined);
       expect(isDomAvailable()).toEqual(false);
     });
 
     it("should return false with document create not available", () => {
-      windowSpy.mockImplementation(() => ({
-        document: undefined,
-      }));
+      Object.defineProperty(global, "window", {
+        value: {
+          document: undefined,
+        },
+        writable: true,
+      });
       expect(isDomAvailable()).toEqual(false);
     });
   });
